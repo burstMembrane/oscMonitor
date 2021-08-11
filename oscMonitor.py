a425cb68f299a8100d8b11dcb90f9d5affb8be85
Liam Power <liamfpower@gmail.com>
2021-08-11 22:20:57 +1000

    first commit

---
+++ oscMonitor.py
@@ -0,0 +1,60 @@
+#!/osc/bin/python3
+# binds to specific port and ip combination parsed in as args,
+# then logs all recieved osc messages to the console using a custom logger
+
+import argparse
+import math
+import logging
+from pythonosc import dispatcher
+from pythonosc import osc_server
+from colorama import init
+from colorama import Fore, Back, Style
+import sys
+init()
+
+logging.basicConfig(
+    format='[ %(asctime)s.%(msecs)03d ] %(message)s', encoding='utf-8', level=logging.DEBUG, datefmt=f"%d-%m-%y %H:%M:%S")
+
+
+def print_volume_handler(unused_addr, args, volume):
+    print("[{0}] ~ {1}".format(args[0], volume))
+
+
+def print_compute_handler(unused_addr, args, volume):
+    try:
+        print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+    except ValueError:
+        pass
+
+
+def log(address, *args):
+    if len(args) == 1:
+        logging.info(
+            f" [{args[0]}]{Fore.GREEN}{address}: {Fore.RED}{str(args[1]).ljust()} {Fore.BLUE}[{type(args[1]).__name__}] {Style.RESET_ALL}")
+    else:
+        logging.info(
+            f"{Fore.CYAN}[ {':'.join(args[0])} ] {Fore.GREEN}{address}: {Fore.RED}{str(args[1])} {Fore.BLUE}[{type(args[1]).__name__}] {Style.RESET_ALL}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip",
+                        default="127.0.0.1", help="Listener IP address")
+    parser.add_argument("--port",
+                        type=int, default=5005, help="OSC Port", required=True)
+    parser.add_argument("--address",
+                        type=str, default="/*", help="OSC Address to listen for")
+    args = parser.parse_args()
+
+    dispatcher = dispatcher.Dispatcher()
+    dispatcher.map(args.address, log, args.ip, str(args.port))
+
+    server = osc_server.ThreadingOSCUDPServer(
+        (args.ip, args.port), dispatcher)
+    logging.info("{} Listening on {}:{} {}".format(Fore.GREEN,
+                                                   server.server_address[0], server.server_address[1], Style.RESET_ALL))
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print(f"{Fore.RED}Quitting...{Style.RESET_ALL}")
+        sys.exit(0)
